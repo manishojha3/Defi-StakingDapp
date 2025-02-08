e7bc5940c53e9554257cfe8c99036fcf767827c9
Manish Ojha <ojhamanish83290@gmail.com>
2025-02-08 14:54:23 +0530

    Add staking functionality and reward system

---
+++ client/src/utils/connectWallet.jsx
@@ -0,0 +1,42 @@
+import {ethers,Contract} from "ethers";
+import stakingAbi from "../ABI/stakingAbi.json"
+import stakeTokenAbi from "../ABI/stakeTokenAbi.json";
+
+
+export const connectWallet = async()=>{
+    try{
+       let [signer,provider,stakingContract,stakeTokenContract,chainId]=[null,null,null,null,null];
+       if(window.ethereum===null){
+          throw new Error("Metamsk is not installed");
+       }
+       const accounts = await window.ethereum.request({
+        method:'eth_requestAccounts'
+       })
+
+       let chainIdHex= await window.ethereum.request({
+        method:'eth_chainId'
+       })
+       chainId= parseInt(chainIdHex,16)
+       
+       let selectedAccount =accounts[0];
+       if(!selectedAccount){
+        throw new Error("No ethereum accounts available")
+       } 
+
+       provider = new ethers.BrowserProvider(window.ethereum);
+       signer = await provider.getSigner();
+
+       const stakingContractAddress="0x29628A39D0Cf4b32A7014F6D57b74cd36B917605"
+       const stakeTokenContractAddress="0x4A7811bC565Abc88d409D08C931b47170D8836AA"
+
+       stakingContract= new Contract(stakingContractAddress,stakingAbi,signer);
+       stakeTokenContract=new Contract(stakeTokenContractAddress,stakeTokenAbi,signer);
+
+       return {provider,selectedAccount,stakeTokenContract,stakingContract,chainId}
+
+    }catch(error){
+        console.error(error);
+        throw error
+    }
+    
+}
